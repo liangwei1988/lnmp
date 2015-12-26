6c6b1e98492375b80a57c3828764a4098c02a3c8
liangwei1988 <362253310@qq.com>
2015-12-26 21:33:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -87,5 +87,17 @@ server {
 }
 EOF
 fi
+VOLUME_HOME="/data/mysql"
 
+if [[ ! -d $VOLUME_HOME/mysql ]]; then
+    echo "=> An empty or uninitialized MariaDB volume is detected in $VOLUME_HOME"
+    echo "=> Installing MariaDB ..."
+    mysql_install_db > /dev/null 2>&1
+    echo "=> Done!"  
+    /create_mariadb_admin_user.sh
+else
+    echo "=> Using an existing volume of MariaDB"
+fi
+
+exec mysqld_safe
 /usr/bin/supervisord -n -c /etc/supervisord.conf
